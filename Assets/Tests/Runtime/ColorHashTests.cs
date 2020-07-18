4fec207f21fab1a0736d3c278baf1442c0915ad4
Dmitriy Trifonov <trifonov.dmitriy.m@gmail.com>
2020-07-18 23:28:57 +0700

    Break two tests, both editor and runtime ones

--- Assets/Tests/Runtime/ColorHashTests.cs
+++ Assets/Tests/Runtime/ColorHashTests.cs
@@ -7,7 +7,7 @@ namespace Tests.Runtime
         [Test]
         [TestCase("Hello World", 40152618)]
         [TestCase("Hello Superman", -1291352238)]
-        [TestCase("Hello Batman", -1885369767)]
+        [TestCase("Hello Batman", -18853697671)]
         public void HashCodeTest(string message, int hashCode)
         {
             var actual = ColorHash.ComputeHashFromString(message);
