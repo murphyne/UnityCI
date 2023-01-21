b6b33c5ef6c8c267c76072bf3cc4a1d9aa2943aa
Dmitriy Trifonov <trifonov.dmitriy.m@gmail.com>
2023-01-21 13:59:10 +0700

    Move ComputeHashColor to ColorHash

--- Assets/Scripts/ColorHash.cs
+++ Assets/Scripts/ColorHash.cs
@@ -2,6 +2,16 @@
 
 public static class ColorHash
 {
+    public static Color ComputeHashColor(string message)
+    {
+        var rgba = ComputeHashFromString(message);
+        var r = (rgba >> 24 & 0xFF) / 256f;
+        var g = (rgba >> 16 & 0xFF) / 256f;
+        var b = (rgba >>  8 & 0xFF) / 256f;
+        var hashColor = new Color(r, g, b);
+        return hashColor;
+    }
+
     public static int ComputeHashFromString(string message)
     {
         var hash = Hash128.Compute(message);
--- Assets/Scripts/MaterialColorHash.cs
+++ Assets/Scripts/MaterialColorHash.cs
@@ -15,17 +15,7 @@ public class MaterialColorHash : MonoBehaviour
     private void Update()
     {
         var str = message + Time.time;
-        var color = ComputeHashColor(str);
+        var color = ColorHash.ComputeHashColor(str);
         _renderer.material.SetColor(_colorProperty, color);
-    }
-
-    private static Color ComputeHashColor(string message)
-    {
-        var rgba = ColorHash.ComputeHashFromString(message);
-        var r = (rgba >> 24 & 0xFF) / 256f;
-        var g = (rgba >> 16 & 0xFF) / 256f;
-        var b = (rgba >>  8 & 0xFF) / 256f;
-        var hashColor = new Color(r, g, b);
-        return hashColor;
     }
 }
