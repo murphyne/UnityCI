b0372566fa1aa1d7a254a2bf7d3a2515e9531b5e
Dmitriy Trifonov <trifonov.dmitriy.m@gmail.com>
2023-01-22 17:07:06 +0700

    Extract test cases to TestCaseSource

--- Assets/Tests/Editor/ColorHashTests.cs
+++ Assets/Tests/Editor/ColorHashTests.cs
@@ -1,19 +1,24 @@
-﻿using NUnit.Framework;
+﻿using System.Collections;
+using NUnit.Framework;
 using UnityEngine;
 
 namespace Tests.Editor
 {
     public class ColorHashTests
     {
+        private static readonly IEnumerable TestCases = new[]
+        {
+            new TestCaseData("Hello World", new Color32(0x02, 0x64, 0xad, 0xff)),
+            new TestCaseData("Hello Superman", new Color32(0xb2, 0x07, 0x86, 0xff)),
+            new TestCaseData("Hello Batman", new Color32(0x8e, 0x9e, 0x89, 0xff)),
+        };
+
         [Test]
-        [TestCase("Hello World", 0x02, 0x64, 0xad, 0xff)]
-        [TestCase("Hello Superman", 0xb2, 0x07, 0x86, 0xff)]
-        [TestCase("Hello Batman", 0x8e, 0x9e, 0x89, 0xff)]
-        public void HashCodeTest(string message, byte r, byte g, byte b, byte a)
+        [TestCaseSource(typeof(ColorHashTests), nameof(TestCases))]
+        public void HashCodeTest(string message, Color32 expected)
         {
             TestContext.AddFormatter<Color32>(Color32Formatter);
 
-            Color32 expected = new Color32(r, g, b, a);
             Color32 actual = ColorHash.ComputeHashColor(message);
 
             Assert.AreEqual(expected, actual);
