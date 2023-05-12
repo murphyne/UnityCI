b5c431cd7355620a7b32b28346fb4f7897490479
Dmitriy Trifonov <trifonov.dmitriy.m@gmail.com>
2023-05-13 01:16:09 +0600

    Add documentation comments to CommandLineHelpers

--- Assets/Editor/CommandLineHelpers.cs
+++ Assets/Editor/CommandLineHelpers.cs
@@ -1,5 +1,20 @@
+/// <summary>
+/// Contains helper methods for getting the command-line arguments
+/// passed to the Unity application.
+/// </summary>
 public static class CommandLineHelpers
 {
+    /// <summary>
+    /// Returns index of an argument with given name.
+    /// </summary>
+    /// <remarks>
+    /// Expects only one occurence of the argument.
+    /// </remarks>
+    /// <param name="name">Name of the argument.</param>
+    /// <returns>
+    /// Index of the argument.
+    /// If the argument is not found, returns -1.
+    /// </returns>
     public static int GetArgIndex(string name)
     {
         var args = System.Environment.GetCommandLineArgs();
@@ -13,6 +28,11 @@ public static class CommandLineHelpers
         return -1;
     }
 
+    /// <summary>
+    /// Returns value of an argument at given index.
+    /// </summary>
+    /// <param name="index">Index of the argument.</param>
+    /// <returns>Value of the argument.</returns>
     public static string GetArgValue(int index)
     {
         var args = System.Environment.GetCommandLineArgs();
